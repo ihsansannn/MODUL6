8e2470b616005b2497c2e98836f698595e60d6e4
ihsansann <ihsansannn@gmail.com>
2018-04-02 11:58:00 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/whoami/muhamadihsansusanto_1202144220_modul6/model/User.java
@@ -0,0 +1,33 @@
+package com.example.whoami.muhamadihsansusanto_1202144220_modul6.model;
+
+/**
+ * Created by whoami on 4/1/2018.
+ */
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
